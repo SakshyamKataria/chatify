84673bb5d329025bb4a6735781d4cb8b1bcdc3f2
SakshyamKataria <sakshyamkataria@gmail.com>
2026-02-18 01:58:20 +0530

    deployment correction

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import axiosInstance from '../lib/axios';
 import toast from 'react-hot-toast';
 import { io } from 'socket.io-client';
 
-const baseURL = "http://localhost:3000"; // Replace with your backend URL
+
 
 export const useAuthStore = create((set, get) => ({  //setter and getter arguments, mostly we use setter
     authUser: null,
