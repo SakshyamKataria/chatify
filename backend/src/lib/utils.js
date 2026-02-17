f4f84baf6448408d98b7fc97d69c40f911b095f2
SakshyamKataria <sakshyamkataria@gmail.com>
2026-02-18 02:14:51 +0530

    jwt resolving

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -14,9 +14,10 @@ export const generateToken = (userId,res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-        secure: process.env.NODE_ENV === "production",
+        sameSite: "none",   // FORCE NONE
+        secure: true,       // MUST be true with sameSite none
     });
+
 
     return token;
 }
